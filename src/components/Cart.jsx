443b4890696f06f45eb168cada1c21aba41a5147
Jackson Barker <jcrb45@hotmail.com>
2024-03-21 14:47:22 -0500

    migrated to using useReducer

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,4 @@
-import { useContext, useReducer } from 'react';
+import { useContext } from 'react';
 import { CartContext } from '../store/shopping-cart-context.jsx';
 
 export default function Cart() {
